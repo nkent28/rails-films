669c65cc94346b0b29f48dea1dd862b1db8913eb
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-09-25 02:14:35 +1200

    ordered movie years in descending order

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -7,12 +7,13 @@ channels.keys().forEach(channels)
 const results = document.querySelector('#results');
 
 const searchMovies = (keyword) => {
-const apiUrl = `https://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`; //keyword is a search param
-fetch(apiUrl) //request the api
-  .then(response => response.json()) //parsed the json
+const apiUrl = `https://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`;
+fetch(apiUrl)
+  .then(response => response.json())
   .then((data) => {
-    // console.log(data);
-    data.Search.forEach((movie) => { //iterated over the array of movies and created strings
+    unordered_list = data.Search
+    ordered_list = unordered_list.sort((a, b) => parseFloat(b.Year) - parseFloat (a.Year));
+    ordered_list.forEach((movie) => {
       const newMovie = `<li>
       <a href="https://www.imdb.com/title/${movie.imdbID}/">
       <img src="${movie.Poster}" />
@@ -20,12 +21,10 @@ fetch(apiUrl) //request the api
       <p><strong>${movie.Title}</strong></p>
       <p><strong><em>${movie.Year}</em></strong></p>
       </li>`
-      results.insertAdjacentHTML('beforeend', newMovie) //data.Search is the key to call, the api is limiting the results to 10
-    // inserted each movie to the list, to the end of the list
+      results.insertAdjacentHTML('beforeend', newMovie)
     });
   });
 };
-
 
   const searchForm = document.querySelector('#search-movies')
     searchForm.addEventListener('submit', (event) => {
